d5636798f3ea63657b23b70118b413ba1f220651
PeterNat <peternath280@gmail.com>
2023-11-28 13:57:49 +0700

    test commit branch menu

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo kelompok',
       theme: ThemeData(
         // This is the theme of your application.
         //
